dbe7a48cb342d85f27bc93cbbc8b08f08c18c093
George Sharkey <footballsharke@gmail.com>
2017-12-05 22:17:50 -0800

    adds headlines to stories

--- readWeb.py
+++ readWeb.py
@@ -26,14 +26,14 @@ CNN_link = CNN_S_addy
 f = urllib.urlopen(CNN_link)
 CNN_headline = f.read()
 #looks for headline
-if "pg-headline" in CNN_headline:
-    start_index = CNN_headline.find("pg-headline")
+if "title" in CNN_headline:
+    start_index = CNN_headline.find("title")
     CNN_headline = CNN_headline[start_index :- 1]
-if "\"pg-headline\"" in CNN_headline:
-    start_index = CNN_headline.find("\"pg-headline\"")
-    CNN_headline = CNN_headline[start_index :- 1]
-if "> "in CNN_headline:
+if ">" in CNN_headline:
     start_index = CNN_headline.find(">")
+    CNN_headline = CNN_headline[start_index :- 1]
+if "-"in CNN_headline:
+    start_index = CNN_headline.find("-")
     CNN_headline = CNN_headline[start_index +1 :- 1]
 
 # loop to get just the headline;removes everthing off the end
@@ -43,7 +43,6 @@ for i in CNN_headline:
         break
     else:
         CNN_Headline = CNN_Headline + i
-
 
 #fox
 fox_link = "https://www.foxnews.com"
@@ -66,6 +65,26 @@ for i in fox_front_page:
     else:
         fox_S_addy = fox_S_addy + i
 
+#For headline
+fox_link = fox_S_addy
+f = urllib.urlopen(fox_link)
+fox_Headline = f.read()
+
+if "\"title\"" in fox_Headline:
+    start_index = fox_Headline.find("\"title\"")
+    fox_Headline = fox_Headline[start_index:-1]
+
+if ":" in fox_Headline:
+    start_index = fox_Headline.find(":")
+    fox_Headline = fox_Headline[start_index +3 :-1]
+fox_headline=""
+for i in fox_Headline:
+    if i == "\"":
+        break
+    else:
+        fox_headline = fox_headline + i
+
+
 #Huffingtonpost
 huff_link = "https://www.huffingtonpost.com"
 f = urllib.urlopen(huff_link)
@@ -86,6 +105,28 @@ for i in huff_front_page:
         break
     else:
         huff_S_addy = huff_S_addy + i
+#for headline
+#huffingtonpost likes to throw 403 errors so we use huff_front_page to limit http requests
+
+
+if "-title__" in huff_front_page:
+    start_index = huff_front_page.find("-title__")
+    huff_front_page = huff_front_page[start_index:-1]
+
+
+if ">" in huff_front_page:
+    start_index = huff_front_page.find(">")
+    huff_front_page = huff_front_page[start_index + 1:-1]
+
+huff_Headline = ""
+
+
+for i in huff_front_page:
+    if i == "<":
+        break
+    else:
+        huff_Headline = huff_Headline + i
+
 
 #New York Times
 nyt_link = "https://www.nytimes.com/"
@@ -107,24 +148,47 @@ for i in nyt_front_page:
         break
     else:
         nyt_S_addy = nyt_S_addy + i
-"""
-print(nyt_S_addy)
-print(huff_S_addy)
-print(fox_S_addy)
-#print(CNN_front_page)
-print(CNN_S_addy)
-"""
+
+f = urllib.urlopen(nyt_S_addy)
+nyt_headline = f.read()
+#gets headline
+
+if "title" in nyt_headline:
+    start_index = nyt_headline.find("title")
+    nyt_headline = nyt_headline[start_index:-1]
+if ">" in nyt_headline:
+    start_index = nyt_headline.find(">")
+    nyt_headline = nyt_headline[start_index + 1:-1]
+
+nyt_Headline = ""
+for i in nyt_headline:
+    if i == "-":
+        break
+    else:
+        nyt_Headline = nyt_Headline + i
+
 print("""<!DOCTYPE html>
+<br>
+<link rel="stylesheet" href="./CSS/default.css">
 <h1> NEWS </h1>
-<img src=\"./photos/news.jpg\" >
 <br>
-<p1> <a href=\" """ + CNN_S_addy + """\">CNN </a> </p1>
+<p1> CNN: </p1>
 <br>
-<p1> Headline: """ + CNN_Headline + """
+<p1> <a href=\" """ + CNN_S_addy + """\"> Headline: """ + CNN_Headline + """ </a> </p1>
 <br>
-<p2><a href=\" """ + fox_S_addy + """ \"> FOX </a> </p2>
 <br>
-<p3><a href=\" """ + huff_S_addy + """ \"> HUFFINGTON POST </a> </p3>
+<p2> Fox: </p2>
 <br>
-<p4> <a href=\" """ + nyt_S_addy +""" \"> NEW YORK TIMES </a> </p4>
+<p2><a href=\" """ + fox_S_addy + """ \"> Headline: """ + fox_headline + """ </a> </p2>
+<br>
+<br>
+<p3> HUFFINGTON POST: </p3>
+<br>
+<p3><a href=\" """ + huff_S_addy + """ \"> Headline: """ + huff_Headline + """ </a> </p3>
+<br>
+<br>
+<p4> NEW YORK TIMES </p4>
+<br>
+<p4> <a href=\" """ + nyt_S_addy +""" \"> Headline: """ + nyt_Headline+ """ </a> </p4>
+</body>
 """)
